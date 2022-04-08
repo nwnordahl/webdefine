9375d8e40393ce3ced233fbe8f149e8a5232d147
nwnordahl <nicolaiwnordahl@gmail.com>
2022-04-08 22:20:24 +0200

    Make projects content

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-/* Koden for å lage loope igjennom prosjektene skal skrives her */
+const projects = document.querySelector(".projects");
+
+for (let i = 1; i <= 8; i++) {
+  projects.innerHTML += `<div class="project">
+                           <h2>Prosjekt ${i}</h2>
+                           <i class="fa-solid fa-code fa-2xl"></i>
+                         </div>`;
+}
